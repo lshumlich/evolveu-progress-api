085d17b489d1e7dd236da4514326720b5b219ccd
larry <lshumlich@gmail.com>
2019-02-08 18:42:58 -0700

    Update Questions for second cohort

--- sql/data.py
+++ sql/data.py
@@ -1,31 +1,28 @@
-var1 = """
-a
-b
-c
-d
-e
-"""
-
 questions = """
-Logic@Basic logic (If/Then/Else/Case/loops), ignoring language
-Basic DS@Basic data structures (Maps / Arrays)
-Python@Python
+HTML-CSS@HTML/CSS
+JS Logic@JavaScript Basic Logic (if/then/else/case/loops), maps arrays
+JS Events-DOM@JavaScript Events / DOM
 git@git
-HTML@HTML
-CSS@CSS
-OO@Object-Oriented Concepts
-TDD@Test-Driven Development
-User Exp@User Experience (Web Design)
-App Server@App Servers
-Web Services@Web Services
-Basic JS@a) Basic JavaScript
-JS Events@b) JavaScript Events
-ES6-7@c) ES6, ES7
 ReactJS@ReactJS
-NodeJS@NodeJS
+JS TDD@JavaScript Test-Driven Development
+JS OO@JavaScript Object-Oriented & State
+JS Alg@JavaScript Algorithms
+JS JSON@JavaScript JSON
+Py Logic@Python Basic Logic
+Py Env@Python Environment
+Py File@Python File IO
+Py Excel@Python Excel
+Py TDD@Python TDD
+Py Flask@Python Flask
+Py API@Python API
 Data Struc@Database Structures / Concepts / Normalization / Joins / Keys / Concurrency 
 SQL@SQL
-Mentor@Mentoring
+Career Dev@Career Development
+Critical Thinking@Critical Thinking
+Systems Thinking@Systems Thinking
+Creative Thinking@Creative Thinking
+Team Collaboration@Team Collaboration
 Problem Solv@Problem Solving (Debugging)
+Mentor@Mentoring
 PM@Project Management Skills (Scope, Expectation, Schedule, Budget, Burndown chart)
 """
